df050305ad7873001b45bde0507fb1b4a88b091d
MarkoJ0621 <markolemesos@gmail.com>
2025-02-07 22:42:09 -0500

    shc

---
+++ frontend/scripts.js
@@ -0,0 +1,18 @@
+let onboardItems = document.getElementsByClassName("onboarditem");
+function moveToTracking() {
+    for (let i = 0; i < onboardItems.length; i++) {
+        onboardItems[i].style.visibility = "hidden";
+    }
+
+}
+const Mood = {
+    Happy: 2,
+    Neutral: 1,
+    Sad: 0
+
+};
+class {
+
+
+
+}
