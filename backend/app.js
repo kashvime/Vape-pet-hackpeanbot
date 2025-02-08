52e525d83b1bdf4f35191df1d7659fb6f1ceb6c8
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2025-02-08 10:58:47 -0500

    mongodb connection
    Just set up a mongodb connection and the .env files with the credentials for the connection.

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,15 @@ const path = require('path');
 const defaultPort = 3000
 require('dotenv').config(); //this activates the ability to parse the .env file
 
+
+//automatically parse any incoming requests into a JSON format
+app.use(express.json());
+
+
+//this section sets up the MongoDB connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const DB_USERNAME = process.env.db_username;
+const DB_PASSWORD = process.env.db_password;
+
+const uri = "mongodb+srv://Databaseuser1:<db_password>@vape-cluster.okno4.mongodb.net/?retryWrites=true&w=majority&appName=Vape-Cluster"
