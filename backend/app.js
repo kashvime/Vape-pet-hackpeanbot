d6f4a961cd77672906b9a6e2f6f0e6ebdb3b916a
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2025-02-08 10:10:40 -0500

    Dockerfile
    Added backend Dockerfile and a bunch of import statements for app.js

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const defaultPort = 3000
+require('dotenv').config(); //this activates the ability to parse the .env file
+
