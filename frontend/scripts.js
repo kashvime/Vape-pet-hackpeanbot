383010c149ce4e6e497506aaf53cfb438961efff
MarkoJ0621 <markolemesos@gmail.com>
2025-02-07 22:52:37 -0500

    basic stuff <33

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -9,10 +9,32 @@ const Mood = {
     Happy: 2,
     Neutral: 1,
     Sad: 0
+}
 
-};
-class {
+class Vape {
+    name;
+    puffs;
+    cost;
+}
 
+//make json object to send objects to back-end!!
 
+class TrackingPurchase {
+    constructor(date, mood, vape) {
+        this.date = date
+        this.mood = mood;
+        this.vape = vape;
+    }
+    date;
+    mood;  //mood enum
+    vape;  //vape class to call back
+}
 
+class TrackingThrowaway {
+    constructor(date, mood) {
+        this.date = date;
+        this.mood = mood;
+    }
+    date;
+    mood; //mood enum
 }
